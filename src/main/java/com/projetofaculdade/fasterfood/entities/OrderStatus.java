40b9f7e428a07d9d8f8c0adebab654848d302caf
Jhonathan Moraes <114410326+JhonathanMoraes@users.noreply.github.com>
2023-10-03 12:34:14 -0300

    primeiro pull request (#1)
    * criação das classes
    
    * criação das classes

--- src/main/java/com/projetofaculdade/fasterfood/entities/OrderStatus.java
+++ src/main/java/com/projetofaculdade/fasterfood/entities/OrderStatus.java
@@ -3,11 +3,10 @@ package com.projetofaculdade.fasterfood.entities;
 
 public enum OrderStatus {
     
-    // TODO inserir os valores do tipo int
-    RECEIVED(),
-    CONFIRMED(),
-    PREPARED(),
-    DELIVERED(),
-    CANCELED();
+    RECEIVED,
+    CONFIRMED,
+    PREPARED,
+    DELIVERED,
+    CANCELED;
     
 }
